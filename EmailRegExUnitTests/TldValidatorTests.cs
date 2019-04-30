2977dbea7617bddd30a685730fe67bef0e79651f
Jared A Barneck <rhyous@yahoo.com>
2019-04-30 08:26:15 -0600

    Added stub missing file

---
+++ EmailRegExUnitTests/TldValidatorTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using EmailRegEx;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EmailRegExUnitTests
+{
+    [TestClass]
+    public class TldValidatorTests
+    {
+        [TestMethod]
+        public void TldValidator_Test()
+        {
+
+        }
+    }
+}
